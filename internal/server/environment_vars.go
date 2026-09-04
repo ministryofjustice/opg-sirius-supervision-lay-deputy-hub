1f3768583c99fcbd8dc6223d6e04d908f90e020a
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2026-09-04 14:29:46 +0100

    sfs-684a test in dev env

--- internal/server/environment_vars.go
+++ internal/server/environment_vars.go
@@ -3,18 +3,18 @@ package server
 import "os"
 
 type EnvironmentVars struct {
-	Port            string
-	WebDir          string
-	SiriusURL       string
+	Port   string
+	WebDir string
+	//SiriusURL       string
 	Prefix          string
 	SiriusPublicURL string
 }
 
 func NewEnvironmentVars() EnvironmentVars {
 	return EnvironmentVars{
-		Port:            getEnv("PORT", "1234"),
-		WebDir:          getEnv("WEB_DIR", "web"),
-		SiriusURL:       getEnv("SIRIUS_URL", "http://localhost:8080"),
+		Port:   getEnv("PORT", "1234"),
+		WebDir: getEnv("WEB_DIR", "web"),
+		//SiriusURL:       getEnv("SIRIUS_URL", "http://localhost:8080"),
 		SiriusPublicURL: getEnv("SIRIUS_PUBLIC_URL", ""),
 		Prefix:          getEnv("PREFIX", ""),
 	}
